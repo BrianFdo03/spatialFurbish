30da768a9ab6975862e352b7a9772340121a60d6
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2026-03-29 11:37:36 +0530

    fixed payload issue on safari browser

--- server/src/index.js
+++ server/src/index.js
@@ -55,8 +55,8 @@ app.use(
   }),
 );
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 // Use routes
